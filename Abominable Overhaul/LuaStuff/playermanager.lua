ce68b96226b99dfbc0431d846eabae6cf099909f
offyerrocker <offyerrocker88@gmail.com>
2024-11-12 19:58:53 -0800

    even more debug

---
+++ Abominable Overhaul/LuaStuff/playermanager.lua
@@ -0,0 +1,3 @@
+Hooks:PostHook(PlayerManager,"sync_carry_data","fsalfsd",function(self,unit, carry_id, carry_multiplier, dye_initiated, has_dye_pack, dye_value_multiplier, position, dir, throw_distance_multiplier_upgrade_level, zipline_unit, peer_id)
+	Print("peer_id",peer_id,"tier",throw_distance_multiplier_upgrade_level)
+end)
