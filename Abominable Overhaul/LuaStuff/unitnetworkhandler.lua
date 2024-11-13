f4a8a7a4a2d9845d41b0005c991da3f835704a47
offyerrocker <offyerrocker88@gmail.com>
2024-11-12 19:56:34 -0800

    more debug stuff

--- Abominable Overhaul/LuaStuff/unitnetworkhandler.lua
+++ Abominable Overhaul/LuaStuff/unitnetworkhandler.lua
@@ -1,3 +1,7 @@
-Hooks:PostHook(UnitNetworkHandler,"sync_carry_data",function(self,unit, carry_id, carry_multiplier, dye_initiated, has_dye_pack, dye_value_multiplier, position, dir, throw_distance_multiplier_upgrade_level, zipline_unit, peer_id, sender)
-	Print("sync carry data() sender",sender,"peer_id",peer_id,"tier",throw_distance_multiplier_upgrade_level)
+Hooks:PostHook(UnitNetworkHandler,"sync_carry_data","asdfjsd",function(self,unit, carry_id, carry_multiplier, dye_initiated, has_dye_pack, dye_value_multiplier, position, dir, throw_distance_multiplier_upgrade_level, zipline_unit, peer_id, sender)
+	Print("sync_carry data() sender",sender,"peer_id",peer_id,"tier",throw_distance_multiplier_upgrade_level)
+end)
+
+Hooks:PostHook(UnitNetworkHandler,"server_drop_carry","asdfjsdd",function(self, carry_id, carry_multiplier, dye_initiated, has_dye_pack, dye_value_multiplier, position, dir, throw_distance_multiplier_upgrade_level, zipline_unit, sender)
+	Print("server_drop_carry data() sender",sender,"tier",throw_distance_multiplier_upgrade_level)
 end)
